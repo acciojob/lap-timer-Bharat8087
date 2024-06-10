c6b2e6a5e03ce0ea2bbd0e1bd17b4a46e82fbc3d
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-06-10 14:15:53 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LapTimer from './LapTimer';
+
+function App() {
+  return (
+    <div className="App">
+      <LapTimer />
+    </div>
+  );
+}
+
+export default App;
